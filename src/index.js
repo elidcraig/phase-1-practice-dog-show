49398bb8e64d0ebb6a973feeeef062f992775efe
Eli Craig <elidcraig@gmail.com>
2022-05-04 14:22:04 -0700

    initial fetch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+    fetch('http://localhost:3000/dogs')
+        .then(resp => resp.json())
+        .then(dogsArray => {
+            console.log(dogsArray)
+        })
 })
