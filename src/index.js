9bf1ec3c55214687a0227ecf42814ab26a2d229c
Eli Craig <elidcraig@gmail.com>
2022-05-04 14:46:45 -0700

    render dogs to table

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 const dogTableBody = document.querySelector('#table-body')
+const dogEditorForm = document.querySelector('#dog-form')
 
 function renderDogTable(dogsArray) {
+    dogsArray.forEach(dogObj => {
+        const dogRow = document.createElement('tr')
+
+        const dogNameCell = document.createElement('td')
+        const dogBreedCell = document.createElement('td')
+        const dogSexCell = document.createElement('td')
+        const editButtonCell = document.createElement('td')
+        const editButton = document.createElement('button')
+
+        dogNameCell.textContent = dogObj.name 
+        dogBreedCell.textContent = dogObj.breed 
+        dogSexCell.textContent = dogObj.sex
+        editButton.textContent = 'Edit'
+        editButtonCell.appendChild(editButton)
+        
+        dogRow.appendChild(dogNameCell)
+        dogRow.appendChild(dogBreedCell)
+        dogRow.appendChild(dogSexCell)
+        dogRow.appendChild(editButtonCell)
+
+        dogTableBody.appendChild(dogRow)
+    })
+    
 
 }
 
